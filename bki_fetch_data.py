17587d129d64263b08de27768ff6486a542b2130
Nmobki <nmo@bki.dk>
2022-03-17 08:11:19 +0100

    Update bki_fetch_data.py
    Moved server information to seperate script

--- bki_fetch_data.py
+++ bki_fetch_data.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import urllib
+import Server_information as si
 import pandas as pd
-from sqlalchemy import create_engine
-import pyodbc
 
 
 # =============================================================================
 # Variables for query connections
 # =============================================================================
-server_04 = 'sqlsrv04'
-db_ds = 'BKI_Datastore'
-con_ds = pyodbc.connect(f'DRIVER=SQL Server;SERVER={server_04};DATABASE={db_ds};autocommit=True')
-params_ds = urllib.parse.quote_plus(f'DRIVER=SQL Server Native Client 11.0;SERVER={server_04};DATABASE={db_ds};Trusted_Connection=yes')
-engine_ds = create_engine(f'mssql+pyodbc:///?odbc_connect={params_ds}')
-cursor_ds = con_ds.cursor()
-
-server_nav = r'SQLSRV03\NAVISION'
-db_nav = 'NAV100-DRIFT'
-con_nav = pyodbc.connect(f'DRIVER=SQL Server;SERVER={server_nav};DATABASE={db_nav};Trusted_Connection=yes')
-params_nav = urllib.parse.quote_plus(f'DRIVER=SQL Server Native Client 11.0;SERVER={server_nav};DATABASE={db_nav};Trusted_Connection=yes')
-engine_nav = create_engine(f'mssql+pyodbc:///?odbc_connect={params_nav}')
-
-server_probat = '192.168.125.161'
-db_probat = 'BKI_IMP_EXP'
-con_probat = pyodbc.connect(f'DRIVER=SQL Server;SERVER={server_probat};DATABASE={db_probat};uid=bki_read;pwd=Probat2016')
-params_probat = urllib.parse.quote_plus(f'DRIVER=SQL Server Native Client 11.0;SERVER={server_probat};DATABASE={db_probat};Trusted_Connection=yes')
-engine_probat = create_engine(f'mssql+pyodbc:///?odbc_connect={params_probat}')
-
+con_ds = si.con_ds
+con_nav = si.con_nav
+con_probat = si.con_probat
 
     
 # Convert list into string for SQL IN operator
