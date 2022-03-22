efe161e155461a54ac2da7dbb2c9e2a04776ae98
Nmobki <nmo@bki.dk>
2022-03-22 14:07:36 +0100

    Update bki_functions.py

--- bki_functions.py
+++ bki_functions.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+import pandas as pd
+import bki_server_information as bsi
+
+
 
 # Convert list into string for SQL IN operator
 def string_to_sql(list_with_values: list) -> str:
@@ -11,7 +16,6 @@ def string_to_sql(list_with_values: list) -> str:
     ----------
     list_with_values : list
         List containing all values which need to be joined into one string
-
     \n Returns
     -------
     String with comma separated values.
@@ -21,3 +25,102 @@ def string_to_sql(list_with_values: list) -> str:
         return ''
     else:
         return "'{}'".format("','".join(list_with_values))
+
+
+# Check if script is supposed to exit. 0 value = exit
+def get_exit_check(value: int):
+    """Calls sys.exit() if input value == 0"""
+    if value == 0:
+        sys.exit()
+    else:
+        pass
+
+# Read top 1 record of blend request log
+def get_ds_blend_request() -> pd.DataFrame():
+    """
+    Returns a pandas dataframe with the top 1 record from BKI_Datastore which has not been started or completed.
+    Returns columns from database with same names as they exist in source table.
+    If no non-started or non-completed records exists, sys.exit() is called to terminate script.
+    """
+    query = 'SELECT TOP 1 * FROM [cof].[Receptforslag_log] WHERE [Status] = 0'
+    df = pd.read_sql(query, bsi.con_ds)
+    # Call function to exit script if no data exists from query
+    get_exit_check(len(df))
+    # If script has not been terminated, return dataframe with data
+    return df
+
+get_ds_blend_request()
+
+
+def get_spot_available_amounts() -> pd.DataFrame():
+    """ Returns a dataframe with all available coffee from SPOT. """
+    query = """ SELECT PL.[Document No_] AS [Kontraktnummer],PL.[Location Code] AS [Lokation]
+                    ,PL.[Outstanding Quantity] AS [Beholdning]
+            FROM [dbo].[BKI foods a_s$Purchase Line] AS PL
+            INNER JOIN [dbo].[BKI foods a_s$Purchase Header] AS PH
+            	ON PL.[Document No_] = PH.[No_]
+            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+            	ON PL.[No_] = I.[No_]
+            WHERE PL.[Type] = 2	AND PL.[Location Code] = 'SPOT'
+            	AND PL.[Outstanding Quantity] > 0 AND PH.[Kontrakt] = 1
+            	AND I.[Item Category Code] = 'RÅKAFFE'
+            UNION ALL
+            SELECT ILE.[Coffee Batch No_] ,ILE.[Location Code]
+                ,SUM(ILE.[Remaining Quantity]) AS [Qty]
+            FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+            	ON ILE.[Item No_]= I.[No_]
+            WHERE ILE.[Remaining Quantity] > 0 AND ILE.[Location Code] = 'SPOT'
+            	AND I.[Item Category Code] = 'RÅKAFFE'
+            GROUP BY ILE.[Coffee Batch No_] ,ILE.[Location Code] """
+    df = pd.read_sql(query, bsi.con_nav)
+    return df
+
+def get_havn_available_amounts() -> pd.DataFrame():
+    """ Returns a dataframe with all available coffee from AARHUSHAVN & EKSLAGER2. """
+    query = """ SELECT ILE.[Coffee Batch No_] AS [Kontraktnummer]
+                ,ILE.[Location Code] AS [Lokation] ,SUM(ILE.[Remaining Quantity]) AS [Qty]
+            FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+            	ON ILE.[Item No_]= I.[No_]
+            WHERE ILE.[Remaining Quantity] > 0 AND ILE.[Location Code] IN ('AARHUSHAVN','EKSLAGER2')
+            	AND I.[Item Category Code] = 'RÅKAFFE'
+            GROUP BY ILE.[Coffee Batch No_] ,ILE.[Location Code] """
+    df = pd.read_sql(query, bsi.con_nav)
+    return df
+
+def get_udland_available_amounts() -> pd.DataFrame():
+    """ Returns a dataframe with all available coffee from Udland. """
+    query = """ SELECT PL.[Document No_] AS [Kontraktnummer],PL.[Location Code] AS [Lokation]
+                    ,PL.[Outstanding Quantity] AS [Beholdning]
+            FROM [dbo].[BKI foods a_s$Purchase Line] AS PL
+            INNER JOIN [dbo].[BKI foods a_s$Purchase Header] AS PH
+            	ON PL.[Document No_] = PH.[No_]
+            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+            	ON PL.[No_] = I.[No_]
+            WHERE PL.[Type] = 2	AND PL.[Location Code] = 'UDLAND'
+            	AND PL.[Outstanding Quantity] > 0 AND PH.[Kontrakt] = 1
+            	AND I.[Item Category Code] = 'RÅKAFFE' """
+    df = pd.read_sql(query, bsi.con_nav)
+    return df
+
+def get_afloat_available_amounts() -> pd.DataFrame():
+    """ Returns a dataframe with all available coffee from AARHUSHAVN & EKSLAGER2. """
+    query = """ SELECT ILE.[Coffee Batch No_] AS [Kontraktnummer]
+                ,ILE.[Location Code] AS [Lokation] ,SUM(ILE.[Remaining Quantity]) AS [Qty]
+            FROM [dbo].[BKI foods a_s$Item Ledger Entry] AS ILE
+            INNER JOIN [dbo].[BKI foods a_s$Item] AS I
+            	ON ILE.[Item No_]= I.[No_]
+            WHERE ILE.[Remaining Quantity] > 0 AND ILE.[Location Code] = 'AFLOAT'
+            	AND I.[Item Category Code] = 'RÅKAFFE'
+            GROUP BY ILE.[Coffee Batch No_] ,ILE.[Location Code] """
+    df = pd.read_sql(query, bsi.con_nav)
+    return df
+
+
+
+
+
+
+
+
