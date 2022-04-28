46aff0c98f521d517134a87ed74e81416f7453aa
Nmobki <nmo@bki.dk>
2022-04-28 11:19:42 +0200

    Update bki_server_information.py

--- bki_server_information.py
+++ bki_server_information.py
@@ -28,11 +28,3 @@ con_probat = pyodbc.connect(f'DRIVER=SQL Server;SERVER={server_probat};DATABASE=
 # Filepaths
 # =============================================================================
 filepath_report = r'\\appsrv07\Python filer\Receptforslag'
-filepath_models = r'\\appsrv07\Python filer\Receptforslag\Modeller'
-filepath_models_archive = r'\\appsrv07\Python filer\Receptforslag\Modeller\Arkiv'
-
-# =============================================================================
-# Model names
-# =============================================================================
-name_model_include_robusta = 'flavor_predictor_robusta.sav'
-name_model_exclude_robusta = 'flavor_predictor_no_robusta.sav'
