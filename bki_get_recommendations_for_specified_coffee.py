1839b4a32bb7bd826c69a2d9594af976a9dc271b
Nmobki <nmo@bki.dk>
2022-06-01 15:29:09 +0200

    Update bki_get_recommendations_for_specified_coffee.py

--- bki_get_recommendations_for_specified_coffee.py
+++ bki_get_recommendations_for_specified_coffee.py
@@ -13,14 +13,17 @@ import time
 start_time = time.time()
 
 # Temp variables, change to values from query
-items = list(range(11))
+items = list(range(8))
 request_item = 3
 request_proportion = 5
 
 
 
-def get_blends_with_proportions(required_item: int, min_proportion: int, available_items: list, number_of_components: int) -> list:
+def get_blends_with_proportions(required_item:int, min_proportion:int, available_items:list, number_of_components:int) -> list:
 
+    padding_placeholder = (7 - number_of_components) * [-1]
+    padding_proportions = (7 - number_of_components) * [0] 
+    padding = list(zip(list(padding_placeholder), list(padding_proportions)))
 
     number_of_available_items = len(available_items)
     # If a blend recommendation is requested with more components than components are available, terminate.
@@ -37,13 +40,14 @@ def get_blends_with_proportions(required_item: int, min_proportion: int, availab
         return None
 
     # Dictionary with lists of possible percentages for components that are not that main component - hardcoded increments
+    # Min proportion of a component is 5, and all component proportions are incremented by 5.
     ranges_proportions_remaining_items = {
-         2: [i for i in range(5,101 - min_proportion,5)]
-        ,3: [i for i in range(5,96 - min_proportion,5)]
-        ,4: [i for i in range(5,91 - min_proportion ,5)]
-        ,5: [i for i in range(5,86 - min_proportion,5)]
-        ,6: [i for i in range(5,81 - min_proportion,5)]
-        ,7: [i for i in range(5,76 - min_proportion,5)]}
+         2: [i for i in range(5, 101 - min_proportion, 5)]
+        ,3: [i for i in range(5, 96 - min_proportion, 5)]
+        ,4: [i for i in range(5, 91 - min_proportion, 5)]
+        ,5: [i for i in range(5, 86 - min_proportion, 5)]
+        ,6: [i for i in range(5, 81 - min_proportion, 5)]
+        ,7: [i for i in range(5, 76 - min_proportion, 5)]}
   
     # Remove the requested contract from the list of possible contracts
     available_items = [item for item in available_items if item != required_item]  
@@ -66,6 +70,7 @@ def get_blends_with_proportions(required_item: int, min_proportion: int, availab
     
     # Combine proportions and contracts into final list
     blends_prop = list(itertools.product(blends, proportions))
+    blends_prop = [blend + padding for blend in blends_prop]
     
     # Clear variables from memory
     del blends,proportions,missing_proportions
@@ -73,21 +78,135 @@ def get_blends_with_proportions(required_item: int, min_proportion: int, availab
     return blends_prop
 
 
-for i in range(1,8):
-    xyz = get_blends_with_proportions(
-        request_item
-        ,5
-        ,items
-        ,i)
-    print("i: " + str(i) + "\n", len(xyz)) if xyz else None
+# for i in range(1,8):
+#     xyz = get_blends_with_proportions(
+#         request_item
+#         ,5
+#         ,items
+#         ,i)
+#     print("i: " + str(i) + "\n", len(xyz)) if xyz else None
+
+xyz = get_blends_with_proportions(
+    request_item
+    ,request_proportion
+    ,items
+    ,5)
+
+
+def get_useable_blends(blends:list, target_flavor_profile:list, target_color:int, flavor_predictor
+                       ,flavors_list, allowed_target_deviation:float = 0.5) -> list:
+    
+    #TODO! padding
+    #TODO! Ensure function exits if it can't run properly
+    
+    predicted_blend_profiles = [tpo.taste_pred(blend,flavor_predictor, flavors_list, target_color) for blend in blends]
+    
+    
+    return predicted_blend_profiles
+
+
+                    # predicted_flavor_profile = tpo.taste_pred(
+                    #     blend
+                    #     ,flavor_predictor
+                    #     ,flavors_list
+                    #     ,110)
 
 # =============================================================================
-# xyz = get_blends_with_proportions(
-#     request_item
-#     ,request_proportion
-#     ,items
-#     ,5)
+# TEMP for testing of permutations
 # =============================================================================
+# Add locations to dictionary for later
+dict_locations = {
+    "SILOER": 1
+    ,"WAREHOUSE": 1
+    ,"AARHUSHAVN": 1
+    ,"SPOT": 0
+    ,"AFLOAT": 0
+    ,"UDLAND": 0}
+
+# Minimum available amount of coffee for an item to be included
+min_quantity = 1000
+
+
+# Different types of certifications
+dict_certifications = {
+    "Sammensætning": "Blandet"
+    ,"Fairtrade": 1
+    ,"Økologi": 1
+    ,"Rainforest": 1
+    ,"Konventionel": 1}
+
+
+# Get all available quantities available for use in production.
+df_available_coffee = bf.get_all_available_quantities(
+    dict_locations
+    ,min_quantity
+    ,dict_certifications
+    ,True)
+# Remove rows with na, we need values for all parameters. Reset index afterwards
+df_available_coffee.dropna(subset=["Syre","Aroma","Krop","Eftersmag"],inplace=True)
+df_available_coffee.reset_index(drop=True, inplace=True)
+contracts_list = df_available_coffee["Kontraktnummer"].to_list()
+# model for flavor predictor
+model_name = "flavor_predictor_no_robusta.sav"
+flavor_predictor = joblib.load(model_name)
+flavor_columns = ["Syre","Aroma","Krop","Eftersmag"]
+flavors_list = df_available_coffee[flavor_columns].to_numpy()
+target_flavor_list = [6,6,7,6]
+
+
+
+
+
+test_value = get_useable_blends(xyz,target_flavor_list,110,flavor_predictor,flavors_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
